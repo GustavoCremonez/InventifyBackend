b6425b4155c390444504d71ac576b2f03477aae3
Gustavo Cremonez <gustavocremonezgc@gmail.com>
2025-04-18 18:29:06 -0300

    fix(product/category): enable auth

--- InvetifyBackend.Api/Controllers/CategoryController.cs
+++ InvetifyBackend.Api/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace InventifyBackend.Api.Controllers
 {
     [Route("api/[controller]")]
-    // [Authorize]
+    [Authorize]
     [ApiController]
     public class CategoryController : ControllerBase
     {
